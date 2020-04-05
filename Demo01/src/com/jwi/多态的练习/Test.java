7a38bf289d0dabd86a7149bb0468c57306170a79
liuhaomiao <haomiao.liu@jwis.cn>
2020-04-05 16:57:02 +0800

    文件更新

--- Demo01/src/com/jwi/多态的练习/Test.java
+++ Demo01/src/com/jwi/多态的练习/Test.java
@@ -1,14 +1,21 @@
 package com.jwi.多态的练习;
 
+import java.text.SimpleDateFormat;
+
 public class Test {
     public static void main(String[] args) {
 
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("");
+
+    }
+
+    private static void bb() {
         Sb aa = new Sb();
 //        Usb usb1= new Jp();
 
 //        Bjb bjb = new Bjb();
 //        bjb.shiyongUSB(usb);
         aa.dianji();
-
     }
 }
